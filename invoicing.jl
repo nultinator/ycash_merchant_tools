ccb1c1e7fb50c929b22db0238092064946d99d1f
nultinator <72562693+nultinator@users.noreply.github.com>
2021-11-10 23:26:32 -0500

    Update invoicing.jl

--- invoicing.jl
+++ invoicing.jl
@@ -2,5 +2,9 @@ include("ycash.jl")
 function getinvoice()
   goods = ["enter", "goods", "here"]
   yec_price = [1, 2, 3]
-  DataFrame(Dict(:goods => goods, :yec_price => yec_price))
+  df = DataFrame(Dict(:goods => goods, :yec_price => yec_price))
+  payto = read(`./ycash-cli z_getnewaddress`, String)
+  Total = sum(df[!, "yec_price"])
+  println(df)
+  println("Please pay $Total YEC to $payto")
 end
